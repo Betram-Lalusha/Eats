03f95ecf00b70091200e05316dcb3ca8ff843d4a
Betram Lalusha <betramlalusha@fb.com>
2022-06-28 12:55:50 -0700

    navigation to details activity enabled

--- app/src/main/java/com/example/eats/Adapters/UserProfileAdapter.java
+++ app/src/main/java/com/example/eats/Adapters/UserProfileAdapter.java
@@ -1,6 +1,7 @@
 package com.example.eats.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
+import com.example.eats.Activities.DetailActivity;
 import com.example.eats.Models.Post;
 import com.example.eats.R;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +38,15 @@ public class UserProfileAdapter extends ArrayAdapter<Post> {
         Post post = getItem(position);
         ImageView userPost = listitemView.findViewById(R.id.postImage);
         Glide.with(getContext()).load(post.getMedia().getUrl()).into(userPost);
+
+        userPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DetailActivity.class);
+                intent.putExtra("post", Parcels.wrap(post));
+                getContext().startActivity(intent);
+            }
+        });
         return listitemView;
     }
 }
