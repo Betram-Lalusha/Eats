39ee6a38b5c73358d30d890186290f03e258c85e
Betram Lalusha <betramlalusha@fb.com>
2022-06-28 12:29:05 -0700

    grid view of user posts added

--- app/src/main/java/com/example/eats/Adapters/UserProfileAdapter.java
+++ app/src/main/java/com/example/eats/Adapters/UserProfileAdapter.java
@@ -4,54 +4,37 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.example.eats.Models.Post;
 import com.example.eats.R;
-import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserProfileAdapter extends RecyclerView.Adapter<UserProfileAdapter.ViewHolder> {
-    Context mContext;
-    List<Post> mPosts;
+public class UserProfileAdapter extends ArrayAdapter<Post> {
 
-    public UserProfileAdapter(Context context, List<Post> posts) {
-        this.mPosts = posts;
-        this.mContext = context;
+    public UserProfileAdapter(@NonNull Context context, int resource, @NonNull List<Post> posts) {
+        super(context, 0, posts);
     }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.single_post, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Post post = mPosts.get(position);
-        holder.bind(post);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mPosts.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView mUserPost;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            mUserPost = itemView.findViewById(R.id.postImage);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listitemView = convertView;
+        if (listitemView == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.user_profile_post, parent, false);
         }
-
-        public void bind(Post post) {
-            Glide.with(mContext).load(post.getMedia().getUrl()).into(mUserPost);
-        }
+        Post post = getItem(position);
+        ImageView userPost = listitemView.findViewById(R.id.postImage);
+        Glide.with(getContext()).load(post.getMedia().getUrl()).into(userPost);
+        return listitemView;
     }
 }
+
--- app/src/main/java/com/example/eats/Fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/eats/Fragments/UserProfileFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,6 +29,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -35,6 +37,7 @@ public class UserProfileFragment extends Fragment {
 
     TextView mUserBio;
     TextView mUserName;
+    GridView mGridView;
     Button mLogOutButton;
     List<Post> mUserPosts;
     ParseUser mCurrentUser;
@@ -49,11 +52,14 @@ public class UserProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @NonNull Bundle savedInstanceState) {
+        mUserPosts = new LinkedList<Post>();
         mUserBio = view.findViewById(R.id.bio);
         mCurrentUser = ParseUser.getCurrentUser();
         mUserName = view.findViewById(R.id.username);
+        mGridView = view.findViewById(R.id.gridView);
         mLogOutButton = view.findViewById(R.id.logOutButton);
         mUserProfilePic = view.findViewById(R.id.userProfilePic);
+        mUserProfileAdapter = new UserProfileAdapter(getContext(),0, mUserPosts);
 
         mLogOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +76,31 @@ public class UserProfileFragment extends Fragment {
         mUserBio.setText(mCurrentUser.getString("bio"));
         Glide.with(getContext()).load(mCurrentUser.getParseFile("userProfilePic").getUrl()).into(mUserProfilePic);
 
+        mGridView.setAdapter(mUserProfileAdapter);
+
+        //query for user posts
+        getUserPosts();
+
+    }
+
+    private void getUserPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.USER);
+        query.whereEqualTo(Post.USER, ParseUser.getCurrentUser());
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null) {
+                    Log.i("HOME", "something went wrong obtaining posts " + e);
+                }
+
+                // save received posts to list and notify adapter of new data
+                mUserPosts.addAll(posts);
+                mUserProfileAdapter.notifyDataSetChanged();
+               // mScrollListener.resetState();
+            }
+        });
     }
 
 }
