16651ba68a943f9a68882b66ae1c7de6bf70f915
Betram Lalusha <betramlalusha@fb.com>
2022-07-03 18:25:37 -0700

    City Model added

---
+++ app/src/main/java/com/example/eats/Models/City.java
@@ -0,0 +1,36 @@
+package com.example.eats.Models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import org.parceler.Parcel;
+
+import java.util.Date;
+
+@Parcel
+@ParseClassName("City")
+public class City extends ParseObject {
+    public static final String NAME = "name";
+    public static final String IMAGE_URL = "imageUrl";
+
+    public City() {}
+
+    public String getName() {
+        return getString(NAME);
+    }
+
+    public void setName(String cityName) {
+        put(NAME, cityName);
+    }
+
+    public String getImageUrl() {
+        return getString(IMAGE_URL);
+    }
+
+    public void setImageUrl(String cityImageUrl) {
+        put(IMAGE_URL, cityImageUrl);
+    }
+
+}
