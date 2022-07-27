a44689b37249fff98cf451077d5804c7950405b6
Betram Lalusha <betramlalusha@fb.com>
2022-07-27 10:50:47 -0700

    users can now log out in settings activity

--- app/src/main/java/com/example/eats/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/eats/Activities/SettingsActivity.java
@@ -2,15 +2,36 @@ package com.example.eats.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.eats.R;
+import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button mLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().hide();
+
+        mLogoutButton = findViewById(R.id.logOutButton);
+
+        //log out user
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
