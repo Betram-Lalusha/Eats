38b7e9ec3df4b5a3acddbdcbbbb18098cf4fb7e7
Betram Lalusha <betramlalusha@fb.com>
2022-07-27 11:34:26 -0700

    user can now turn dark mode on and off

--- app/src/main/java/com/example/eats/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/eats/Activities/SettingsActivity.java
@@ -1,18 +1,25 @@
 package com.example.eats.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.example.eats.R;
 import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Switch mSwicthMode;
     Button mLogoutButton;
+    SharedPreferences mSharedPreferences = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         mLogoutButton = findViewById(R.id.logOutButton);
+        mSwicthMode = findViewById(R.id.switchModeButton);
 
         //log out user
         mLogoutButton.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +41,34 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //swicthing between dark mode and light mode
+        mSharedPreferences = getSharedPreferences("night", 0);
+        Boolean booleanValue = mSharedPreferences.getBoolean("night mode", false);
+
+        if(booleanValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            mSwicthMode.setChecked(true);
+        }
+
+        mSwicthMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    mSwicthMode.setChecked(true);
+                    SharedPreferences.Editor editor = mSharedPreferences.edit();
+                    editor.putBoolean("night mode", true);
+                    editor.commit();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    mSwicthMode.setChecked(false);
+                    SharedPreferences.Editor editor = mSharedPreferences.edit();
+                    editor.putBoolean("night mode", false);
+                    editor.commit();
+                }
+            }
+        });
+
     }
 }
