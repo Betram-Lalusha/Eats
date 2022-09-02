bbcf7c630a46b45558edb099b0ca94acf7a17c04
Betram Lalusha <lalushabetm@gmail.com>
2022-09-02 10:23:48 -0700

    text change listeners added to edit texts

--- app/src/main/java/com/example/eats/Activities/ChangeEmail.java
+++ app/src/main/java/com/example/eats/Activities/ChangeEmail.java
@@ -3,14 +3,19 @@ package com.example.eats.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.eats.R;
+import com.parse.ParseUser;
 
 public class ChangeEmail extends AppCompatActivity {
 
     EditText mOldEmail;
     EditText mNewEmail;
+    ParseUser mCurrentUser;
     EditText mConfirmNewEmail;
 
     @Override
@@ -21,8 +26,59 @@ public class ChangeEmail extends AppCompatActivity {
         getSupportActionBar().hide();
 
         mOldEmail = findViewById(R.id.oldEmail);
+        mCurrentUser = ParseUser.getCurrentUser();
         mNewEmail = findViewById(R.id.enterNewEmail);
         mConfirmNewEmail = findViewById(R.id.confirmNewEmail);
 
+        mOldEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mNewEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mConfirmNewEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
