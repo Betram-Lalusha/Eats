f978a8dff9930f719f5d8bc11a4b90be38d3e7d0
Betram Lalusha <lalushabetm@gmail.com>
2022-09-02 10:11:48 -0700

    adding new elements

--- app/src/main/java/com/example/eats/Activities/ChangeEmail.java
+++ app/src/main/java/com/example/eats/Activities/ChangeEmail.java
@@ -3,10 +3,15 @@ package com.example.eats.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.eats.R;
 
 public class ChangeEmail extends AppCompatActivity {
+
+    EditText mOldEmail;
+    EditText mNewEmail;
+    EditText mConfirmNewEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,10 @@ public class ChangeEmail extends AppCompatActivity {
         setContentView(R.layout.activity_change_email);
 
         getSupportActionBar().hide();
+
+        mOldEmail = findViewById(R.id.oldEmail);
+        mNewEmail = findViewById(R.id.enterNewEmail);
+        mConfirmNewEmail = findViewById(R.id.confirmNewEmail);
+
     }
 }
