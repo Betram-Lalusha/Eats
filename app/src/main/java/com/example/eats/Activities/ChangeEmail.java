b55b84c6beec741f3161c8c2393f12d386584b91
Betram Lalusha <lalushabetm@gmail.com>
2022-09-05 19:57:20 -0700

    changing user email enabled

--- app/src/main/java/com/example/eats/Activities/ChangeEmail.java
+++ app/src/main/java/com/example/eats/Activities/ChangeEmail.java
@@ -2,29 +2,37 @@ package com.example.eats.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.eats.Emails.ValidEmailTester;
 import com.example.eats.R;
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class ChangeEmail extends AppCompatActivity {
 
     EditText mOldEmail;
     EditText mNewEmail;
-    String mOldUserEmail;
     ParseUser mCurrentUser;
+    TextView mChangingEmail;
+    TextView mEmailsDontMatch;
     EditText mConfirmNewEmail;
     TextView mInvalidEmailError;
+    ProgressBar mSavingNewEmail;
+    ProgressBar mConfirmingEmail;
     ProgressBar mCheckingNewEmail;
     ProgressBar mCheckingOldEmail;
     TextView mIncorrectEmailError;
+    Button mConfirmEmailChangeButton;
     ValidEmailTester mValidEmailTester = new ValidEmailTester();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +43,17 @@ public class ChangeEmail extends AppCompatActivity {
 
         mOldEmail = findViewById(R.id.oldEmail);
         mCurrentUser = ParseUser.getCurrentUser();
-        mOldUserEmail = mCurrentUser.getEmail();
         mNewEmail = findViewById(R.id.enterNewEmail);
+        mChangingEmail = findViewById(R.id.changingEmail);
+        mSavingNewEmail = findViewById(R.id.savingNewEmail);
+        mEmailsDontMatch = findViewById(R.id.emailsDontMatch);
+        mConfirmingEmail = findViewById(R.id.confirmingEmail);
         mConfirmNewEmail = findViewById(R.id.confirmNewEmail);
         mCheckingNewEmail = findViewById(R.id.checkingNewEmail);
         mCheckingOldEmail = findViewById(R.id.checkingOldEmail);
         mInvalidEmailError = findViewById(R.id.invalidEmailError);
         mIncorrectEmailError = findViewById(R.id.inCorrectEmailError);
+        mConfirmEmailChangeButton = findViewById(R.id.confirmEmailChangeButton);
 
         mOldEmail.addTextChangedListener(new TextWatcher() {
             @Override
@@ -58,7 +70,7 @@ public class ChangeEmail extends AppCompatActivity {
                     return;
                 }
                 mCheckingOldEmail.setVisibility(View.VISIBLE);
-                if(!mOldUserEmail.equals(s.toString())) {
+                if(!mCurrentUser.getEmail().equals(s.toString())) {
                     mNewEmail.setVisibility(View.INVISIBLE);
                     mIncorrectEmailError.setVisibility(View.VISIBLE);
                 } else {
@@ -114,12 +126,66 @@ public class ChangeEmail extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().isEmpty()) {
+                    mEmailsDontMatch.setVisibility(View.INVISIBLE);
+                    mConfirmingEmail.setVisibility(View.INVISIBLE);
+                    mConfirmEmailChangeButton.setVisibility(View.INVISIBLE);
+                    return;
+                }
 
+                mConfirmingEmail.setVisibility(View.VISIBLE);
+                if(!mNewEmail.getText().toString().equals(s.toString())) {
+                    mEmailsDontMatch.setVisibility(View.VISIBLE);
+                    mConfirmingEmail.setVisibility(View.VISIBLE);
+                    mConfirmEmailChangeButton.setVisibility(View.INVISIBLE);
+                } else {
+                    mEmailsDontMatch.setVisibility(View.INVISIBLE);
+                    mConfirmingEmail.setVisibility(View.INVISIBLE);
+                    mConfirmEmailChangeButton.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        mConfirmEmailChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeUserEmail();
+            }
+        });
+    }
+
+    private void changeUserEmail() {
+        mChangingEmail.setVisibility(View.VISIBLE);
+        mSavingNewEmail.setVisibility(View.VISIBLE);
+
+        mCurrentUser.setEmail(mConfirmNewEmail.getText().toString());
+
+        //save info
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    mChangingEmail.setTextColor(Color.RED);
+                    mChangingEmail.setText("The following error occured: " + e.getMessage() + ". Please try again");
+                    mSavingNewEmail.setVisibility(View.INVISIBLE);
+                    return;
+                }
+
+                //successful
+                mNewEmail.setText("");
+                mOldEmail.setText("");
+                mConfirmNewEmail.setText("");
+                mChangingEmail.setTextColor(Color.GREEN);
+                mChangingEmail.setText("new email saved.");
+                mNewEmail.setVisibility(View.INVISIBLE);
+                mSavingNewEmail.setVisibility(View.INVISIBLE);
+                mConfirmNewEmail.setVisibility(View.INVISIBLE);
+                mConfirmEmailChangeButton.setVisibility(View.INVISIBLE);
             }
         });
     }
