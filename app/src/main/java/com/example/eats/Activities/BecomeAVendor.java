dd5827339c3f4e872be1db8e246ea37194cf37b8
Betram Lalusha <lalushabetm@gmail.com>
2022-11-11 16:24:51 -0800

    adding changes to vendor view

--- app/src/main/java/com/example/eats/Activities/BecomeAVendor.java
+++ app/src/main/java/com/example/eats/Activities/BecomeAVendor.java
@@ -12,5 +12,7 @@ public class BecomeAVendor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_become_avendor);
+
+        getSupportActionBar();
     }
 }
